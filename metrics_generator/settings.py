15b22a97b24d811a60256009f8c3467c29d00129
hyper-glitch <graff049@gmail.com>
2022-12-03 20:22:05 +0300

    feat(settings): move settings for generator

---
+++ metrics_generator/settings.py
@@ -0,0 +1,9 @@
+"""Module for set up metrics generator environment vars."""
+from environs import Env
+
+env = Env()
+env.read_env()
+
+METRICS_GEN_SERVICE_PORT = env.int('MONITORING_SERVICE_PORT', 21122)
+METRICS_GEN_SERVICE_HOST = env.str('MONITORING_SERVICE_HOST', 'localhost')
+METRICS_GEN_ENDPOINT = "generate_metrics"
