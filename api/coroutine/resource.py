a2b5a4db8a5583879a7b2b85776d267548903871
hyper-glitch <graff049@gmail.com>
2022-12-06 22:09:13 +0300

    feat(vars): import cfg vars

--- api/coroutine/resource.py
+++ api/coroutine/resource.py
@@ -3,9 +3,10 @@ import asyncio
 
 import aiohttp
 
-from metrics_generator.settings import (
+from api.cfg import (
     METRICS_GEN_SERVICE_PORT,
     METRICS_GEN_SERVICE_HOST,
+    METRICS_GEN_SERVICE_ENDPOINT,
 )
 
 
@@ -14,11 +15,11 @@ async def get(session, url):
         return await response.text()
 
 
-async def get_resource_task(portion_amount) -> list[str]:
-    url = f"http://{METRICS_GEN_SERVICE_HOST}:{METRICS_GEN_SERVICE_PORT}/generate_metrics/"
+async def get_resource_task(portion_amount: int) -> list[str]:
+    url = f"http://{METRICS_GEN_SERVICE_HOST}:{METRICS_GEN_SERVICE_PORT}/{METRICS_GEN_SERVICE_ENDPOINT}"
 
     async with aiohttp.ClientSession() as session:
-        tasks = [get(session, url=url + str(num)) for num in range(portion_amount)]
+        tasks = [get(session, url=f"{url}/{num}") for num in range(portion_amount)]
         raw_data = await asyncio.gather(*tasks)
 
     return raw_data
