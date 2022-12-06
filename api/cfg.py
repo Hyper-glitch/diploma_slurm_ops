57e8a94e99b203b72f462b8a49a3a61ab6269083
hyper-glitch <graff049@gmail.com>
2022-12-06 22:03:44 +0300

    feat(cfg): add api cfg

---
+++ api/cfg.py
@@ -0,0 +1,9 @@
+"""Module for set up api environment vars."""
+from environs import Env
+
+env = Env()
+env.read_env()
+
+METRICS_GEN_SERVICE_PORT = env.int("MONITORING_SERVICE_PORT", 21122)
+METRICS_GEN_SERVICE_HOST = env.str("MONITORING_SERVICE_HOST", "metrics_generator")
+METRICS_GEN_SERVICE_ENDPOINT = env.str("MONITORING_SERVICE_HOST", "generate_metrics")
