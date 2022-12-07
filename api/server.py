85ebc91213c7ac6404ca2b1a3ec694009875bb1f
hyper-glitch <graff049@gmail.com>
2022-12-07 11:33:21 +0300

    feat(otlp): add span exporter

--- api/server.py
+++ api/server.py
@@ -5,6 +5,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from opentelemetry import trace
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 from api.cfg import OTLP_EXPORTER_AGENT_HOSTNAME, OTLP_EXPORTER_AGENT_PORT
@@ -26,13 +28,14 @@ def get_application():
     app.add_event_handler("startup", tasks.create_start_app_handler(app))
     app.add_event_handler("shutdown", tasks.create_stop_app_handler(app))
     app.include_router(router, prefix="/api")
+    resource = Resource(attributes={"service.name": "service-async_analyze_metrics"})
+    trace.set_tracer_provider(TracerProvider(resource=resource))
     otlp_exporter = OTLPSpanExporter(
         endpoint=f"{OTLP_EXPORTER_AGENT_HOSTNAME}:{OTLP_EXPORTER_AGENT_PORT}",
         insecure=True,
     )
     trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(otlp_exporter))
     FastAPIInstrumentor.instrument_app(app)
-
     return app
 
 
